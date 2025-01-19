dd082d68176f2752ead3ba909784044ad31f6465
Hardik <hardik@Ubuntu.myguest.virtualbox.org>
2025-01-19 06:42:35 +0530

    Writer.sh Fix#2

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 
+# Check if there are required number of parameters.
+# If not, return with exit code 1
 if [ $# -ne 2 ];
 	then
 		echo "Not Enough Parameters Entered. The command takes two parameters.\r\nfilesdr: Path to directory\r\nsearchstr: String to find in the directory"
 		exit 1;
 fi
 
+s
 writefile="$1"
 writestr="$2"
 
-# Try to create the file. If not successful, return with exit code 1.
-if [ $(mkdir -p $writefile) -neq 0 ];
+
+# Extract directory path and try to make directory. If not successful, return with exit code 1.
+directory_extracted=$(dirname "$writefile")
+mkdir -p "$directory_extracted"
+if [ $? -ne 0 ];
 	then
-		echo "Could not create file."
-		exit(1);
+		echo "Could not create directory."
+		echo "Exit code 1"
+		exit 1;
 fi
 
-# Try to create the file. If not successful, return with exit code 1.
-if [ $($writestr > $writefile) -neq 0 ];
+# Try writing the string to the file. If not successful, return with exit code 1.
+echo $writestr > $writefile
+if [ $? -ne 0 ];
 	then
 		echo "Could not write to file."
-		exit(1);
+		echo "Exit code 1"
+		exit 1;
 fi
+
 
