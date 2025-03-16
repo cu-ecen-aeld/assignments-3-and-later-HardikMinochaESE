07a9ecace5a9db9164d53fb3ce40957a6f02e026
Hardik <hardik@Ubuntu.myguest.virtualbox.org>
2025-03-16 17:38:26 -0600

    aesdchar driver test passed

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -8,6 +8,9 @@
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
 
+#include "aesd-circular-buffer.h"
+#include <linux/mutex.h>
+
 #define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
 #undef PDEBUG             /* undef it, just in case */
@@ -23,12 +26,23 @@
 #  define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
 
+// Add these function prototypes before the struct definition
+int aesd_open(struct inode *inode, struct file *filp);
+int aesd_release(struct inode *inode, struct file *filp);
+ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+int aesd_init_module(void);
+void aesd_cleanup_module(void);
+
 struct aesd_dev
 {
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
     struct cdev cdev;     /* Char device structure      */
+    struct mutex lock;    /* Mutex for thread safety */
+    struct aesd_circular_buffer buffer;  /* Circular buffer for storing data */
+    struct aesd_buffer_entry unfinished_entry; /* Buffer for incomplete writes */
 };
 
 
